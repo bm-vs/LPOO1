ec81de933bbbe7156a9a7636e50050888679796a
BSantos22 <bruno.santos1996@gmail.com>
2016-03-30 11:55:51 +0100

    stuff

--- MazeGame/src/gamegui/Gui.java
+++ MazeGame/src/gamegui/Gui.java
@@ -24,6 +24,17 @@ public class Gui {
 	private JFrame frmMazeGame;
 	private JTextField maze_size_input;
 	private JTextField number_dragons_input;
+	private JComboBox dragon_type_input;
+	private JLabel dragon_type;
+	private JLabel estado;
+	private JLabel number_dragons;
+	private JLabel maze_size;
+	private Game game;
+	private JButton move_up;
+	private JButton move_down;
+	private JButton move_left;
+	private JButton move_right;
+	private JTextArea maze_area;
 
 	/**
 	 * Launch the application.
@@ -52,7 +63,7 @@ public class Gui {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		Game game = new Game();
+		game = new Game();
 
 		frmMazeGame = new JFrame();
 		frmMazeGame.getContentPane().setForeground(Color.BLACK);
@@ -61,7 +72,7 @@ public class Gui {
 		frmMazeGame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmMazeGame.getContentPane().setLayout(null);
 
-		JLabel maze_size = new JLabel("Dimens\u00E3o do labirinto");
+		maze_size = new JLabel("Dimens\u00E3o do labirinto");
 		maze_size.setBounds(52, 42, 162, 14);
 		frmMazeGame.getContentPane().add(maze_size);
 
@@ -71,7 +82,7 @@ public class Gui {
 		frmMazeGame.getContentPane().add(maze_size_input);
 		maze_size_input.setColumns(10);
 
-		JLabel number_dragons = new JLabel("N\u00FAmero de drag\u00F5es");
+		number_dragons = new JLabel("N\u00FAmero de drag\u00F5es");
 		number_dragons.setBounds(52, 78, 162, 14);
 		frmMazeGame.getContentPane().add(number_dragons);
 
@@ -81,212 +92,69 @@ public class Gui {
 		number_dragons_input.setBounds(224, 75, 65, 20);
 		frmMazeGame.getContentPane().add(number_dragons_input);
 
-		JLabel estado = new JLabel("You can play!");
+		estado = new JLabel("You can play!");
 		estado.setBounds(62, 536, 150, 14);
 		frmMazeGame.getContentPane().add(estado);
 
-		JLabel dragon_type = new JLabel("Tipo de drag\u00F5es");
+		dragon_type = new JLabel("Tipo de drag\u00F5es");
 		dragon_type.setBounds(51, 115, 162, 14);
 		frmMazeGame.getContentPane().add(dragon_type);
 
-		JComboBox dragon_type_input = new JComboBox();
+		dragon_type_input = new JComboBox();
 		dragon_type_input.setBounds(223, 112, 138, 20);
 		frmMazeGame.getContentPane().add(dragon_type_input);
-		dragon_type_input.addItem("Est·ticos");
+		dragon_type_input.addItem("Est√°ticos");
 		dragon_type_input.addItem("Mover");
 		dragon_type_input.addItem("Mover e adormecer");
 
-		JTextArea maze_area = new JTextArea();
-		maze_area.setFont(new Font("Courier New", Font.PLAIN, 13));
+		maze_area = new JTextArea();
+		maze_area.setFont(new Font("Courier", Font.PLAIN, 13));
 		maze_area.setBounds(52, 155, 422, 379);
 		frmMazeGame.getContentPane().add(maze_area);
 
-		JButton move_up = new JButton("Cima");
-		JButton move_left = new JButton("Esquerda");
-		JButton move_down = new JButton("Baixo");
-		JButton move_right = new JButton("Direita");
+		move_up = new JButton("Cima");
+		move_left = new JButton("Esquerda");
+		move_down = new JButton("Baixo");
+		move_right = new JButton("Direita");
 
 		move_up.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String game_mode;
-				if (dragon_type_input.getSelectedItem().equals("Est·ticos")) {
-					game_mode = "2";
-				} else if (dragon_type_input.getSelectedItem().equals("Mover")) {
-					game_mode = "0";
-				} else {
-					game_mode = "1";
-				}
-
-				switch (game.play(game_mode, "w")) {
-				case 1:
-					maze_area.setText(game.return_board()); // WIN
-					estado.setText("The hero has won!");
-					close();
-					break;
-				case 2:
-					maze_area.setText(game.return_board());
-					estado.setText("The hero has died!"); // LOSE
-					close();
-					break;
-				case 3:
-					maze_area.setText(game.return_board());
-					estado.setText("The dragon has died!"); // DRAGON DIES
-					break;
-				}
-
-				maze_area.setText(game.return_board());
-			}
-
-			protected void close() {
-				maze_area.setText(game.return_board());
-				move_up.setEnabled(false);
-				move_down.setEnabled(false);
-				move_left.setEnabled(false);
-				move_right.setEnabled(false);
-
-			}
-
-		});
+				keyAction("w");
+			}});
 
 		move_up.setBounds(632, 285, 88, 44);
 		frmMazeGame.getContentPane().add(move_up);
 
 		move_down.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String game_mode;
-				if (dragon_type_input.getSelectedItem().equals("Est·ticos")) {
-					game_mode = "2";
-				} else if (dragon_type_input.getSelectedItem().equals("Mover")) {
-					game_mode = "0";
-				} else {
-					game_mode = "1";
-				}
-
-				switch (game.play(game_mode, "s")) {
-				case 1:
-					maze_area.setText(game.return_board()); // WIN
-					estado.setText("The hero has won!");
-					close();
-					break;
-				case 2:
-					maze_area.setText(game.return_board());
-					estado.setText("The hero has died!"); // LOSE
-					close();
-					break;
-				case 3:
-					maze_area.setText(game.return_board());
-					estado.setText("The dragon has died!"); // DRAGON DIES
-					break;
-				}
-
-				maze_area.setText(game.return_board());
-			}
-
-			protected void close() {
-				maze_area.setText(game.return_board());
-				move_up.setEnabled(false);
-				move_down.setEnabled(false);
-				move_left.setEnabled(false);
-				move_right.setEnabled(false);
-
-			}
-		});
+				keyAction("s");
+			}});
+		
 		move_down.setBounds(632, 340, 88, 44);
 		frmMazeGame.getContentPane().add(move_down);
 
 		move_right.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String game_mode;
-				if (dragon_type_input.getSelectedItem().equals("Est·ticos")) {
-					game_mode = "2";
-				} else if (dragon_type_input.getSelectedItem().equals("Mover")) {
-					game_mode = "0";
-				} else {
-					game_mode = "1";
-				}
-
-				switch (game.play(game_mode, "d")) {
-				case 1:
-					maze_area.setText(game.return_board()); // WIN
-					estado.setText("The hero has won!");
-					close();
-					break;
-				case 2:
-					maze_area.setText(game.return_board());
-					estado.setText("The hero has died!"); // LOSE
-					close();
-					break;
-				case 3:
-					maze_area.setText(game.return_board());
-					estado.setText("The dragon has died!"); // DRAGON DIES
-					break;
-				}
-
-				maze_area.setText(game.return_board());
-			}
-
-			protected void close() {
-				maze_area.setText(game.return_board());
-				move_up.setEnabled(false);
-				move_down.setEnabled(false);
-				move_left.setEnabled(false);
-				move_right.setEnabled(false);
-
-			}
-
-		});
+				keyAction("d");
+			}});
+		
 		move_right.setBounds(730, 313, 88, 44);
 		frmMazeGame.getContentPane().add(move_right);
 
 		move_left.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String game_mode;
-				if (dragon_type_input.getSelectedItem().equals("Est·ticos")) {
-					game_mode = "2";
-				} else if (dragon_type_input.getSelectedItem().equals("Mover")) {
-					game_mode = "0";
-				} else {
-					game_mode = "1";
-				}
-
-				switch (game.play(game_mode, "a")) {
-				case 1:
-					maze_area.setText(game.return_board()); // WIN
-					estado.setText("The hero has won!");
-					close();
-					break;
-				case 2:
-					maze_area.setText(game.return_board());
-					estado.setText("The hero has died!"); // LOSE
-					
-					close();
-					break;
-				case 3:
-					maze_area.setText(game.return_board());
-					estado.setText("The dragon has died!"); // DRAGON DIES
-					break;
-				}
-
-				maze_area.setText(game.return_board());
-			}
-
-			protected void close() {
-				maze_area.setText(game.return_board());
-				move_up.setEnabled(false);
-				move_down.setEnabled(false);
-				move_left.setEnabled(false);
-				move_right.setEnabled(false);
-
-			}
-
-		});
+				keyAction("a");
+			}});
+		
 		move_left.setBounds(534, 313, 88, 44);
 		frmMazeGame.getContentPane().add(move_left);
 
 		JButton new_game = new JButton("Gerar novo labirinto");
 		new_game.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-
+				
+				
+		
 				int size = Integer.parseInt(maze_size_input.getText());
 				int dragons = Integer.parseInt(number_dragons_input.getText());
 				boolean accepted = true;
@@ -335,6 +203,8 @@ public class Gui {
 
 					game.maze = new Maze(size);
 					game.hero = new Hero(game.maze);
+					
+					game.dragons.clear();					
 
 					for (int i = 0; i < dragons; i++) {
 						game.dragons.add(new Dragon(game.maze));
@@ -357,6 +227,44 @@ public class Gui {
 		exit.setBounds(584, 92, 178, 43);
 		frmMazeGame.getContentPane().add(exit);
 
+	}
+	
+	private void close() {
+		maze_area.setText(game.return_board());
+		move_up.setEnabled(false);
+		move_down.setEnabled(false);
+		move_left.setEnabled(false);
+		move_right.setEnabled(false);
+	}
+	
+	private void keyAction(String key) {
+		String game_mode;
+		if (dragon_type_input.getSelectedItem().equals(dragon_type_input.getItemAt(0))) {
+			game_mode = "2";
+		} else if (dragon_type_input.getSelectedItem().equals(dragon_type_input.getItemAt(1))) {
+			game_mode = "0";
+		} else {
+			game_mode = "1";
+		}
+
+		switch (game.play(game_mode, key)) {
+		case 1:
+			maze_area.setText(game.return_board()); // WIN
+			estado.setText("The hero has won!");
+			close();
+			break;
+		case 2:
+			maze_area.setText(game.return_board());
+			estado.setText("The hero has died!"); // LOSE
+			close();
+			break;
+		case 3:
+			maze_area.setText(game.return_board());
+			estado.setText("The dragon has died!"); // DRAGON DIES
+			break;
+		}
+
+		maze_area.setText(game.return_board());
 	}
 
 }
